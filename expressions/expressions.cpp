8db96ff6b97d1ec081606ce0b1e79dd0ad20931d
trt111 <tohar05022010@gmail.com>
2026-09-24 17:14:58 +0300

    moved the derive and evaluate functions to the c++ file, added the children set for actually deriving and added a polynomial class. TODO: start adding the correct derivative based on the variable we derive by and add trigonometric and expononets and logarithms classes and then v1 is ready

--- expressions/expressions.cpp
+++ expressions/expressions.cpp
@@ -1,1 +1,71 @@
 #include "expressions.h"
+
+double Add::derive() { // (f+g)'(x) = f'(x) + g'(x)
+	double l_derivative = l_exp->derive();
+	double r_derivative = r_exp->derive();
+	return l_derivative + r_derivative;
+}
+double Add::evaluate() { // (f+g)(x) = f(x) + g(x)
+	double l_evaluate = l_exp->evaluate();
+	double r_evaluate = r_exp->evaluate();
+	return l_evaluate + r_evaluate;
+}
+
+double Subtract::derive() { // (f-g)'(x) = f'(x) - g'(x)
+	double l_derivative = l_exp->derive();
+	double r_derivative = r_exp->derive();
+	return l_derivative - r_derivative;
+}
+double Subtract::evaluate() { // (f-g)(x) = f(x)-g(x)
+	double l_evaluate = l_exp->evaluate();
+	double r_evaluate = r_exp->evaluate();
+	return l_evaluate - r_evaluate;
+}
+
+double Multiply::derive() { // (f*g)'(x) = f'(x)g(x) + g'(x)f(x)
+	double l_derivative = l_exp->derive();
+	double r_derivative = r_exp->derive();
+	double l_evaluate = l_exp->evaluate();
+	double r_evaluate = r_exp->evaluate();
+	return l_derivative * r_evaluate + r_derivative * l_evaluate;
+}
+double Multiply::evaluate() { // (f*g)(x) = f(x)*g(x)
+	double l_evaluate = l_exp->evaluate();
+	double r_evaluate = r_exp->evaluate();
+	return l_evaluate * r_evaluate;
+}
+
+double Divide::derive() { // (f/g)'(x) = (f'(x)g(x) - g'(x)f(x))/(g(x))^2
+	double l_derivative = l_exp->derive();
+	double r_derivative = r_exp->derive();
+	double l_evaluate = l_exp->evaluate();
+	double r_evaluate = r_exp->evaluate();
+	if (r_evaluate == 0) throw "division by zero";
+	return (l_derivative * r_evaluate - r_derivative * l_evaluate) / (r_evaluate * r_evaluate);
+}
+double Divide::evaluate() { // (f/g)(x) = f(x)/g(x)
+	double l_evaluate = l_exp->evaluate();
+	double r_evaluate = r_exp->evaluate();
+	if (r_evaluate == 0) throw "division by zero";
+	return l_evaluate / r_evaluate;
+}
+
+double Polynomial::derive() { // (f^n)'(x) = n*f'(x)*(f(x))^(n-1)
+	double base_evaluate = base->evaluate();
+	if (base_evaluate == 0) return 0;
+	double base_derivative = base->derive();
+	double total = power;
+	for (int i = 0; i < (this->power)-1; i++) {
+		total *= base_evaluate;
+	}
+	return total;
+}
+double Polynomial::evaluate() { // (f^n)(x) = (f(x))^n
+	double base_evaluate = base->evaluate();
+	if (base_evaluate == 0) return 0;
+	double total = 1;
+	for (int i = 0; i < (this->power); i++) {
+		total *= base_evaluate;
+	}
+	return total;
+}
--- expressions/expressions.h
+++ expressions/expressions.h
@@ -1,19 +1,23 @@
 #ifndef EXPRESSIONS_H
 #define EXPRESSIONS_H
-#include <string>
+
+
 #include <iostream>
+#include <string>
+#include <set>
+
 using namespace std;
 
 class Expression
 {
 private:
 	string type;
+	set<Expression*> Ch;
 public:
-	Expression(string type) {
-		this->type = type;
-	}
+	Expression(string type) { this->type = type; }
 	virtual double derive() = 0;
 	virtual double evaluate() = 0;
+	void add_child(Expression* child) { Ch.insert(child); }
 	string get_type() { return type; }
 	virtual ~Expression() {}
 };
@@ -23,141 +27,101 @@ class Number : public Expression
 private:
 	double value;
 public:
-	Number(double value) : Expression("number") {
-		this->value = value;
-	}
+	Number(double value) : Expression("number") { this->value = value; }
 	double derive() { return 0; }
 	double evaluate() { return value; }
 	virtual ~Number() {}
-
+	operator int() { return (int)value; }
+	operator int() { return value; }
 };
 
-class Variable : public Expression 
+class Variable : public Expression
 {
 private:
 	double value;
 public:
-	Variable(double value) : Expression("variable") {
-		this->value = value;
-	}
+	Variable(double value) : Expression("variable") { this->value = value; }
 	double derive() { return 1; }
 	double evaluate() { return value; }
 	virtual ~Variable() {}
 
 };
 
-class Add
+class Add : public Expression
 {
 private:
-	Expression& l_exp;
-	Expression& r_exp;
+	Expression* l_exp;
+	Expression* r_exp;
 
 public:
-	Add(Expression& left, Expression& right) : Expression("addition") {
-		this->l_exp = left;
-		this->r_exp = right;
+	Add(Expression* left, Expression* right) : Expression("addition"), l_exp(left), r_exp(right) {
+		if (l_exp->type != "number") l_exp->add_child(this);
+		if (r_exp->type != "number") r_exp->add_child(this);
 	}
-	virtual double derive() {
-		double l_derivative = l_exp.derive();
-		double r_derivative = r_exp.derive();
-		return l_derivative + r_derivative;
-	}
-	virtual double evaluate() {
-		double l_evaluate = l_exp.evaluate();
-		double r_evaluate = r_exp.evaluate();
-		return l_evaluate + r_evaluate;
-	}
-	virtual ~Add() {
-		delete l_exp;
-		delete r_exp;
-	}
+	double derive();
+	double evaluate();
+	virtual ~Add() { delete l_exp; delete r_exp; }
 };
 
-class Subtract
+class Subtract : public Expression
 {
 private:
-	Expression& l_exp;
-	Expression& r_exp;
+	Expression* l_exp;
+	Expression* r_exp;
 
 public:
-	Subtract(Expression& left, Expression& right) : Expression("subtraction") {
-		this->l_exp = left;
-		this->r_exp = right;
+	Subtract(Expression* left, Expression* right) : Expression("subtraction"), l_exp(left), r_exp(right) { 
+		if (l_exp->type != "number") l_exp->add_child(this);
+		if (r_exp->type != "number") r_exp->add_child(this);
 	}
-	virtual double derive() {
-		double l_derivative = l_exp.derive();
-		double r_derivative = r_exp.derive();
-		return l_derivative - r_derivative;
-	}
-	virtual double evaluate() {
-		double l_evaluate = l_exp.evaluate();
-		double r_evaluate = r_exp.evaluate();
-		return l_evaluate - r_evaluate;
-	}
-	virtual ~Subtract() {
-		delete l_exp;
-		delete r_exp;
-	}
+	double derive();
+	double evaluate();
+	virtual ~Subtract() { delete l_exp; delete r_exp; }
 };
 
-class Multiply
+class Multiply : public Expression
 {
 private:
-	Expression& l_exp;
-	Expression& r_exp;
+	Expression* l_exp;
+	Expression* r_exp;
 
 public:
-	Multiply(Expression& left, Expression& right) : Expression("multiplication") {
-		this->l_exp = left;
-		this->r_exp = right;
+	Multiply(Expression* left, Expression* right) : Expression("multiplication"), l_exp(left), r_exp(right) { 
+		if (l_exp->type != "number") l_exp->add_child(this);
+		if (r_exp->type != "number") r_exp->add_child(this);
 	}
-	virtual double derive() {
-		double l_derivative = l_exp.derive();
-		double r_derivative = r_exp.derive();
-		double l_evaluate = l_exp.evaluate();
-		double r_evaluate = r_exp.evaluate();
-		return l_derivative * r_evaluate + r_derivative * l_evaluate;
-	}
-	virtual double evaluate() {
-		double l_evaluate = l_exp.evaluate();
-		double r_evaluate = r_exp.evaluate();
-		return l_evaluate * r_evaluate;
-	}
-	virtual ~Multiply() {
-		delete l_exp;
-		delete r_exp;
-	}
+	double derive();
+	double evaluate();
+	virtual ~Multiply() { delete l_exp; delete r_exp; }
 };
 
-class Divide
+class Divide : public Expression
 {
 private:
-	Expression& t_exp;
-	Expression& b_exp;
+	Expression* l_exp;
+	Expression* r_exp;
 
 public:
-	Divide(Expression& top, Expression& buttom) : Expression("division") {
-		this->t_exp = top;
-		this->b_exp = buttom;
+	Divide(Expression* left, Expression* right) : Expression("division"), l_exp(left), r_exp(right) { 
+		if (l_exp->type != "number") l_exp->add_child(this);
+		if (r_exp->type != "number") r_exp->add_child(this);
 	}
-	virtual double derive() {
-		double t_derivative = t_exp.derive();
-		double b_derivative = b_exp.derive();
-		double t_evaluate = t_exp.evaluate();
-		double b_evaluate = b_exp.evaluate();
-		if (b_evaluate == 0) throw "division by zero";
-		return (t_derivative * b_evaluate - b_derivative * t_evaluate)/(b_evaluate*b_evaluate);
+	double derive();
+	double evaluate();
+	virtual ~Divide() { delete l_exp; delete r_exp; }
+};
+
+class Polynomial : public Expression
+{
+	Expression* base;
+	unsigned int power;
+public:
+	Polynomial(Expression* base, unsigned int power) : Expression("polynomial"), base(base), power(power) { 
+		if (this->base->type != "number") this->base->add_child(this);
 	}
-	virtual double evaluate() {
-		double t_evaluate = t_exp.evaluate();
-		double b_evaluate = b_exp.evaluate();
-		if (b_evaluate == 0) throw "division by zero";
-		return t_evaluate / b_evaluate;
-	}
-	virtual ~Divide() {
-		delete t_exp;
-		delete b_exp;
-	}
+	double derive();
+	double evaluate();
+	virtual ~Poynomial() { delete base; }
 };
 
 #endif 
